4df2bf430eabcb614df03004ab6aa1a1f0af85b7
Andi-Git <andi.dev94@gmail.com>
2024-12-18 16:27:11 +0100

    changes

---
+++ PerturaboTech.Genesis.WebApi/Data/Migrations/20241218152041_Initial.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PerturaboTech.Genesis.WebApi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FirstName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    MiddleName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    LastName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    PictureUrl = table.Column<string>(type: "text", nullable: false),
+                    IsBlocked = table.Column<bool>(type: "boolean", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    LoginAttemptsCount = table.Column<int>(type: "integer", nullable: false),
+                    Token = table.Column<string>(type: "text", nullable: false),
+                    RefreshToken = table.Column<string>(type: "text", nullable: false),
+                    FrontendTheme = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreatedBy = table.Column<string>(type: "text", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- PerturaboTech.Genesis.WebApi/Program.cs
+++ PerturaboTech.Genesis.WebApi/Program.cs
@@ -2,6 +2,10 @@ using Microsoft.EntityFrameworkCore;
 using PerturaboTech.Genesis.WebApi.Apis.Users;
 using PerturaboTech.Genesis.WebApi.Data;
 using PerturaboTech.Genesis.WebApi.Helpers;
+using PerturaboTech.Genesis.WebApi.Services.Abstractions;
+using PerturaboTech.Genesis.WebApi.Services.Abstractions.Repository;
+using PerturaboTech.Genesis.WebApi.Services.Implementations;
+using PerturaboTech.Genesis.WebApi.Services.Implementations.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
@@ -9,14 +13,21 @@ var services = builder.Services;
 var connectionString = configuration.GetConnectionString("DefaultConnection");
 Ensure.NotNullOrEmpty(connectionString);
 services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
+services.AddScoped<IUserRepository, UserRepository>();
+services.AddScoped<IUserService, UserService>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 services.AddOpenApi();
+services.AddAuthentication();
+services.AddAuthorization();
+services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
 
@@ -25,4 +36,5 @@ app.UseAuthentication();
 app.UseAuthorization();
         
 app.MapUsersEndpoints();
+
 app.Run();
