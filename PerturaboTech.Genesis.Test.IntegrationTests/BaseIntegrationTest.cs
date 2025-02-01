0bdb3866637b77b8e50daf45d120b3210d9ab7fb
Andi Thomaj <60044430+andi-thomaj@users.noreply.github.com>
2025-02-01 21:16:37 +0100

    changes

---
+++ PerturaboTech.Genesis.Test.IntegrationTests/BaseIntegrationTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PerturaboTech.Genesis.Domain.Repositories;
+using PerturaboTech.Genesis.Infrastructure;
+
+namespace PerturaboTech.Genesis.Test.IntegrationTests;
+
+public class BaseIntegrationTest : IClassFixture<IntegrationTestWebApplicationFactory>
+{
+    private readonly IServiceScope _scope;
+    private readonly IUserRepository _userRepository;
+    private readonly DapperContext _dapperContext;
+
+    public BaseIntegrationTest(IntegrationTestWebApplicationFactory factory)
+    {
+        _scope = factory.Services.CreateScope();
+        
+        _userRepository = _scope.ServiceProvider.GetRequiredService<IUserRepository>();
+        _dapperContext = _scope.ServiceProvider.GetRequiredService<DapperContext>();
+    }
+}
---
+++ PerturaboTech.Genesis.Test.IntegrationTests/IntegrationTestWebApplicationFactory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using PerturaboTech.Genesis.Shared.Configurations;
+using PerturaboTech.Genesis.WebApi;
+using Testcontainers.PostgreSql;
+
+namespace PerturaboTech.Genesis.Test.IntegrationTests;
+
+public class IntegrationTestWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+        .WithImage("postgres:latest")
+        .WithDatabase("development_genesis")
+        .WithUsername("postgres")
+        .WithPassword("postgres")
+        .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.Configure<DatabaseOptions>(options =>
+            {
+                options.ConnectionString = _dbContainer.GetConnectionString();
+            });
+        });
+    }
+
+    public Task InitializeAsync() => _dbContainer.StartAsync();
+    public new Task DisposeAsync() => _dbContainer.StopAsync();
+}
